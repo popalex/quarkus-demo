712c426c8fc58d15e8e281794c1f81021216a02f
Alex Popescu <pop.alx@gmail.com>
2023-02-03 08:47:52 +0200

    added logging - maybe

--- src/main/java/org/acme/rest/json/FruitResource.java
+++ src/main/java/org/acme/rest/json/FruitResource.java
@@ -13,25 +13,30 @@ import javax.ws.rs.Path;
 public class FruitResource {
 
     private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+    static System.Logger LOG = System.getLogger(FruitSyncService.class.getName());
 
     public FruitResource() {
         fruits.add(new Fruit("Apple", "Winter fruit"));
         fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+        LOG.log(Level.DEBUG, "FruitResource INIT - adding initial fruits ");
     }
 
     @GET
     public Set<Fruit> list() {
+        LOG.log(Level.DEBUG, "GET fruits");
         return fruits;
     }
 
     @POST
     public Set<Fruit> add(Fruit fruit) {
+        LOG.log(Level.DEBUG, "POST fruits");
         fruits.add(fruit);
         return fruits;
     }
 
     @DELETE
     public Set<Fruit> delete(Fruit fruit) {
+        LOG.log(Level.DEBUG, "DELETE fruits");
         fruits.removeIf(existingFruit -> existingFruit.name.contentEquals(fruit.name));
         return fruits;
     }
